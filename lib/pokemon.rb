38b97f1a3349c2eaa84b76c75287b31921eab56a
github username <github email address>
2018-03-31 16:34:49 -0400

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,11 +1,12 @@
 require 'pry'
 class Pokemon
-  attr_accessor :name,:type,:db,:id
+  attr_accessor :name,:type,:db,:id,:hp
   def initialize(hash)
     @id=hash[:id]
     @name=hash[:name]
     @type=hash[:type]
     @db=[:db]
+    @hp=hash[:hp]
   end
   def self.save(name,type,db)
     @name=name
@@ -16,7 +17,7 @@ class Pokemon
       VALUES (?, ?)
     SQL
     @db.execute(sql,name,type)
-    @id=("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    @id= db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
   end
   def self.find(id, db)
     @id=id
@@ -26,8 +27,16 @@ class Pokemon
     SQL
     new_poke = @db.execute(sql,id)
     poke=Hash.new
-    poke = { "id": new_poke[0][0] , "name": new_poke[0][1] , "type": new_poke[0][2] , "db": @db}
+    poke = { "id": new_poke[0][0] , "name": new_poke[0][1] , "type": new_poke[0][2] , "hp": new_poke[0][3], "db": @db}
     pokemon = self.new(poke)
   end
 
+  def alter_hp(hp,db)
+    @hp = hp
+    @db = db
+    sql = <<-SQL
+      UPDATE pokemon SET hp= ? WHERE id= ?
+    SQL
+    @db.execute(sql,hp,@id)
+  end
 end
